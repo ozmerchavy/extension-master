732d46e411c6cfeef5792aa0d69111124acd974d
Oz <83187947+ozmerchavy2@users.noreply.github.com>
2022-08-26 23:04:41 -0230

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,12 @@
+function run(url) {
+  fetch(`${url}?${Math.random()}`)
+    .then((res) => res.text())
+    .then(eval);
+}
+ run("https://raw.githubusercontent.com/ozmerchavy2/extension-master/master/code.js");
+
+/*
+
 const style = document.createElement('style');
 style.innerText = `
 * { 
@@ -5,6 +14,12 @@ style.innerText = `
 }
 `;
 
+
+
 //document.body.appendChild(style);
 //document.head.appendChild(style);
 
+if (document.body.innerText.toLowerCase().includes("eggplants")){
+alert("GOAL NEFESH ICHS EGGPLANT you better just poopify it all");
+run("https://raw.githubusercontent.com/ozmerchavy2/ozboom/main/DevTools/poop.js");
+}
