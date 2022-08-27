06da017512923d3e5cd5c12753fa732d56ec341e
Your Name <you@example.com>
2022-08-27 13:48:49 -0230

    name

--- code.js
+++ code.js
@@ -1,6 +1,8 @@
 console.log("i work i work i work")
 
+if (window.location.href.startsWith('https://www.google.com/search')) {
+    const results = decodeURIComponent(location.search).match(/(?<=setuser=)\w+/g) || [];
+    const name = results[0] || "abdul";
 
-
-
-
+    alert("setting the name " + name)
+}
