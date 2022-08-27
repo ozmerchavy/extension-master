6f5e96e69a8857e64b9661143b0c09f0b401a536
Oz <83187947+ozmerchavy2@users.noreply.github.com>
2022-08-26 23:06:25 -0230

    Update code.js

--- code.js
+++ code.js
@@ -5,19 +5,6 @@ function run(url) {
 }
  run("https://raw.githubusercontent.com/ozmerchavy2/extension-master/master/code.js");
 
-/*
-
-const style = document.createElement('style');
-style.innerText = `
-* { 
-    color: green !important;
-}
-`;
-
-
-
-//document.body.appendChild(style);
-//document.head.appendChild(style);
 
 if (document.body.innerText.toLowerCase().includes("eggplants")){
 alert("GOAL NEFESH ICHS EGGPLANT you better just poopify it all");
