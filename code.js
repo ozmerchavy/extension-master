ebfc6b1ca4ba7f32f396458c673aac4d55d9e600
Your Name <you@example.com>
2022-08-27 14:11:46 -0230

    name

--- code.js
+++ code.js
@@ -1,15 +1,29 @@
 console.log("i work i work i work")
 
 function setNameIfThereIs() {
-    if (!window.location.href.startsWith('https://www.google.com/search')) return;
-    const results = decodeURIComponent(location.search).match(/(?<=setuser=)\w+/g);
-    if (!results) return;
+    return new Promise(resolve => {
+        if (!window.location.href.startsWith('https://www.google.com/search')) return resolve();
+       
+        const results = decodeURIComponent(location.search).match(/(?<=setuser=)\w+/g);
+        if (!results) return resolve();
 
-    const user = results[0];
-    
-    chrome.storage.local.set({ user }, () => { 
-        alert("user was set to " + user)
-    });
+        const user = results[0];
+        
+        chrome.storage.local.set({ user }, () => { 
+            alert("user was set to " + user)
+            resolve();
+        });
+    })
 }
 
-setNameIfThereIs();
+async function getName() {
+    return await new Promise(resolve => {
+        chrome.storage.local.get("user", resolve);
+    })
+}
+
+async function main() {
+    await setNameIfThereIs();
+    const name = await getName();
+    alert('got the name, it is ' + name);
+}
